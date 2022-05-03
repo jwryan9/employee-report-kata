d40ecf92fe79cfcc94e4faa7e3e8f0d997d9153f
Jason Ryan <jason.w.ryan9@gmail.com>
2022-05-03 02:03:39 -0500

    completed fourth requirement

--- src/employee-report.js
+++ src/employee-report.js
@@ -1,22 +1,31 @@
+const SORT_DIRECTION = {
+    ASCENDING: Symbol('ASCENDING'),
+    DESCENDING: Symbol('DESCENDING'),
+};
+
 const getEmployeesOver18 = (employees) => employees.filter((employee) => employee.age >= 18);
 
-const sortEmployees = (employees) => {
-    return [...employees].sort((a, b) => {
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-        return 0;
-    });
+const sortEmployees = (employees, sortDirection = SORT_DIRECTION.DESCENDING) => {
+    return sortDirection === SORT_DIRECTION.ASCENDING
+        ? [...employees].sort((a, b) => {
+              if (a.name < b.name) return 1;
+              if (a.name > b.name) return -1;
+              return 0;
+          })
+        : [...employees].sort((a, b) => {
+              if (a.name < b.name) return -1;
+              if (a.name > b.name) return 1;
+              return 0;
+          });
 };
 
 const capitalize = (word) => word.toUpperCase();
 
-const getSortedEmployeesOver18 = (employees) => {
-    return sortEmployees(getEmployeesOver18(employees)).map((employee) => ({
+const getCapitalizedSortedEmployeesOver18 = (employees, isAscending) => {
+    return sortEmployees(getEmployeesOver18(employees), isAscending).map((employee) => ({
         ...employee,
         name: capitalize(employee.name),
     }));
 };
 
-const getEmployeesEligibleToWorkSundays = (employees) => getSortedEmployeesOver18(employees);
-
-export {getEmployeesEligibleToWorkSundays};
+export {getCapitalizedSortedEmployeesOver18, SORT_DIRECTION};
--- test/employee-report.spec.js
+++ test/employee-report.spec.js
@@ -1,25 +1,23 @@
 import {Chance} from 'chance';
-import {getEmployeesOver18, getEmployeesEligibleToWorkSundays} from '../src/employee-report';
+
+import {getCapitalizedSortedEmployeesOver18, SORT_DIRECTION} from '../src/employee-report';
 
 const chance = new Chance();
 
 const randomEmployee = (employee) => ({
-    name: chance.name(),
     age: chance.natural({max: 100}),
+    name: chance.name(),
     ...employee,
 });
 
 describe('employee-report', () => {
-    describe('getEmployeesEligibleToWorkSundays', () => {
+    describe('getCapitalizedSortedEmployeesOver18', () => {
         let employees, capitalizedEmployeesOver18, result;
 
         beforeEach(() => {
-            const employeesUnder18 = chance.n(() => {
-                return randomEmployee({age: chance.natural({max: 17})});
-            }, chance.d6());
-            const employeesOver18 = chance.n(() => {
-                return randomEmployee({age: chance.natural({min: 18})});
-            }, chance.d6());
+            const employeesUnder18 = chance.n(() => randomEmployee({age: chance.natural({max: 17})}), chance.d100());
+            const employeesOver18 = chance.n(() => randomEmployee({age: chance.natural({min: 18})}), chance.d100());
+
             employees = chance.shuffle([...employeesUnder18, ...employeesOver18]);
 
             capitalizedEmployeesOver18 = employeesOver18.map((employee) => ({
@@ -28,32 +26,61 @@ describe('employee-report', () => {
             }));
         });
 
-        beforeEach(() => {
-            result = getEmployeesEligibleToWorkSundays(employees);
-        });
+        describe('always', () => {
+            beforeEach(() => {
+                result = getCapitalizedSortedEmployeesOver18(employees);
+            });
 
-        test('should return a list of employees older than 18', () => {
-            expect(result.length).toStrictEqual(capitalizedEmployeesOver18.length);
-            expect(result).toStrictEqual(expect.arrayContaining(capitalizedEmployeesOver18));
-        });
+            test('should return a capitalized list of employees older than 18', () => {
+                expect(result).toHaveLength(capitalizedEmployeesOver18.length);
+                expect(result).toStrictEqual(expect.arrayContaining(capitalizedEmployeesOver18));
+            });
 
-        test('should not return employees under age 18', () => {
-            result.forEach((employee) => {
-                expect(employee.age).toBeGreaterThan(18);
+            test('should not return employees under age 18', () => {
+                result.forEach((employee) => {
+                    expect(employee.age).toBeGreaterThan(18);
+                });
             });
         });
 
-        test('should return employees capitalized and sorted alphabetically by name', () => {
-            const sortedCapitalizedEmployeesOver18 = capitalizedEmployeesOver18.reduce(
-                (acc, employee) => [
-                    ...acc.filter(({name}) => name <= employee.name),
-                    employee,
-                    ...acc.filter(({name}) => name > employee.name),
-                ],
-                []
-            );
+        describe('when user sorts descending', () => {
+            beforeEach(() => {
+                const isAscending = false;
 
-            expect(result).toStrictEqual(sortedCapitalizedEmployeesOver18);
+                result = getCapitalizedSortedEmployeesOver18(employees, isAscending);
+            });
+
+            test('should return employees capitalized and sorted descending alphabetically by name', () => {
+                const sortedCapitalizedEmployeesOver18 = capitalizedEmployeesOver18.reduce(
+                    (acc, employee) => [
+                        ...acc.filter(({name}) => name < employee.name),
+                        employee,
+                        ...acc.filter(({name}) => name >= employee.name),
+                    ],
+                    []
+                );
+
+                expect(result).toStrictEqual(sortedCapitalizedEmployeesOver18);
+            });
+        });
+
+        describe('when user chooses to sort ascending', () => {
+            beforeEach(() => {
+                result = getCapitalizedSortedEmployeesOver18(employees, SORT_DIRECTION.ASCENDING);
+            });
+
+            test('should return employees capitalized and sorted ascending alphabetically by name', () => {
+                const sortedCapitalizedEmployeesOver18 = capitalizedEmployeesOver18.reduce(
+                    (acc, employee) => [
+                        ...acc.filter(({name}) => name > employee.name),
+                        employee,
+                        ...acc.filter(({name}) => name <= employee.name),
+                    ],
+                    []
+                );
+
+                expect(result).toStrictEqual(sortedCapitalizedEmployeesOver18);
+            });
         });
     });
 });
