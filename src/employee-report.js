0e56345de09079345808bd6aba3dc7d6508598ac
Jason Ryan <jason.w.ryan9@gmail.com>
2022-05-03 01:31:06 -0500

    completed third requirement

--- src/employee-report.js
+++ src/employee-report.js
@@ -1,20 +1,22 @@
-const getEmployeesOver18 = (employees) =>
-  employees.filter((employee) => employee.age >= 18);
+const getEmployeesOver18 = (employees) => employees.filter((employee) => employee.age >= 18);
 
 const sortEmployees = (employees) => {
-  return [...employees].sort((a, b) => {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
-  });
+    return [...employees].sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+    });
 };
+
+const capitalize = (word) => word.toUpperCase();
 
 const getSortedEmployeesOver18 = (employees) => {
-  const employeesOver18 = getEmployeesOver18(employees);
-  return sortEmployees(employeesOver18);
+    return sortEmployees(getEmployeesOver18(employees)).map((employee) => ({
+        ...employee,
+        name: capitalize(employee.name),
+    }));
 };
 
-const getEmployeesEligibleToWorkSundays = (employees) =>
-  getSortedEmployeesOver18(employees);
+const getEmployeesEligibleToWorkSundays = (employees) => getSortedEmployeesOver18(employees);
 
-export { getEmployeesOver18, getEmployeesEligibleToWorkSundays };
+export {getEmployeesEligibleToWorkSundays};
--- test/employee-report.spec.js
+++ test/employee-report.spec.js
@@ -1,58 +1,59 @@
-import { Chance } from 'chance';
-import {
-  getEmployeesOver18,
-  getEmployeesEligibleToWorkSundays,
-} from '../src/employee-report';
+import {Chance} from 'chance';
+import {getEmployeesOver18, getEmployeesEligibleToWorkSundays} from '../src/employee-report';
 
 const chance = new Chance();
 
 const randomEmployee = (employee) => ({
-  name: chance.name(),
-  age: chance.natural({ max: 100 }),
-  ...employee,
+    name: chance.name(),
+    age: chance.natural({max: 100}),
+    ...employee,
 });
 
 describe('employee-report', () => {
-  describe('getEmployeesEligibleToWorkSundays', () => {
-    let employees, employeesOver18, employeesUnder18, result;
+    describe('getEmployeesEligibleToWorkSundays', () => {
+        let employees, capitalizedEmployeesOver18, result;
 
-    beforeEach(() => {
-      employeesUnder18 = chance.n(() => {
-        return randomEmployee({ age: chance.natural({ max: 17 }) });
-      }, chance.d6());
-      employeesOver18 = chance.n(() => {
-        return randomEmployee({ age: chance.natural({ min: 18 }) });
-      }, chance.d6());
-      employees = chance.shuffle([...employeesUnder18, ...employeesOver18]);
-    });
+        beforeEach(() => {
+            const employeesUnder18 = chance.n(() => {
+                return randomEmployee({age: chance.natural({max: 17})});
+            }, chance.d6());
+            const employeesOver18 = chance.n(() => {
+                return randomEmployee({age: chance.natural({min: 18})});
+            }, chance.d6());
+            employees = chance.shuffle([...employeesUnder18, ...employeesOver18]);
 
-    beforeEach(() => {
-      result = getEmployeesEligibleToWorkSundays(employees);
-    });
-    test('should return a list of employees older than 18', () => {
-      expect(result.length).toStrictEqual(employeesOver18.length);
-      expect(result).toStrictEqual(expect.arrayContaining(employeesOver18));
-    });
+            capitalizedEmployeesOver18 = employeesOver18.map((employee) => ({
+                ...employee,
+                name: employee.name.toUpperCase(),
+            }));
+        });
 
-    test('should not return employees under age 18', () => {
-      expect(result).not.toContainEqual(employeesUnder18);
-    });
+        beforeEach(() => {
+            result = getEmployeesEligibleToWorkSundays(employees);
+        });
 
-    test('should return results from getEmployeesOver18', () => {
-      expect(result).toStrictEqual(expect.arrayContaining(employeesOver18));
-    });
+        test('should return a list of employees older than 18', () => {
+            expect(result.length).toStrictEqual(capitalizedEmployeesOver18.length);
+            expect(result).toStrictEqual(expect.arrayContaining(capitalizedEmployeesOver18));
+        });
 
-    test('should return employees sorted by name', () => {
-      const sortedEmployeesOver18 = employeesOver18.reduce(
-        (acc, employee) => [
-          ...acc.filter(({ name }) => name <= employee.name),
-          employee,
-          ...acc.filter(({ name }) => name > employee.name),
-        ],
-        []
-      );
+        test('should not return employees under age 18', () => {
+            result.forEach((employee) => {
+                expect(employee.age).toBeGreaterThan(18);
+            });
+        });
 
-      expect(result).toStrictEqual(sortedEmployeesOver18);
+        test('should return employees capitalized and sorted alphabetically by name', () => {
+            const sortedCapitalizedEmployeesOver18 = capitalizedEmployeesOver18.reduce(
+                (acc, employee) => [
+                    ...acc.filter(({name}) => name <= employee.name),
+                    employee,
+                    ...acc.filter(({name}) => name > employee.name),
+                ],
+                []
+            );
+
+            expect(result).toStrictEqual(sortedCapitalizedEmployeesOver18);
+        });
     });
-  });
 });
